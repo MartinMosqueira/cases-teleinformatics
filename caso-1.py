844627e15794259c30653a24bf9841f72b0392b1
MartinMosqueira <tinchomosqueira@gmail.com>
2024-04-11 10:55:06 -0300

    feat(caso 1): :construction: advance network

--- caso-1.py
+++ caso-1.py
@@ -1,55 +1,57 @@
-from mininet.cli import CLI
 from mininet.net import Mininet
-from mininet.node import Host, OVSKernelSwitch, Node
-from mininet.topo import Topo
-from mininet.log import setLogLevel, info
+from mininet.node import RemoteController, OVSKernelSwitch
+from mininet.cli import CLI
+from mininet.node import Node
 
 class CustomSwitch(OVSKernelSwitch):
     OVSVersion = '2.17.9'
 
-class CustomTopology(Topo):
-    def build(self):
-        info('*** Add switches\n')
-        s1 = self.addSwitch('s1', cls=CustomSwitch, failMode='standalone')
+def create_topology():
+    net = Mininet(controller=RemoteController)
 
-        info('*** Add hosts\n')
-        h1 = self.addHost('h1', cls=Host, ip='10.0.1.2/24', defaultRoute='via 10.0.1.1')
-        h2 = self.addHost('h2', cls=Host, ip='10.0.1.254/24', defaultRoute='via 10.0.1.1')
+    # Agregar controlador
+    c0 = net.addController('c0', controller=RemoteController, ip='127.0.0.1', port=6633)
 
-        info('*** Add router\n')
-        r1 = self.addHost('r1', cls=Node, ip='10.0.1.1/24')
-        r1_wan = self.addHost('r1_wan', cls=Node, ip='192.168.100.1/29')
+    # Agregar switch
+    s1 = net.addSwitch('s1', cls=CustomSwitch, failMode='standalone')    
 
+    # Agregar nodos
+    rc = net.addHost('rc', cls=Node, ip=None)
+    r1 = net.addHost('r1', cls=Node, ip=None)
+    h1 = net.addHost('h1', ip='10.0.1.2/24')
+    h2 = net.addHost('h2', ip='10.0.1.254/24')
 
+    # Agregar enlaces
+    net.addLink(h1, s1)
+    net.addLink(h2, s1)
+    net.addLink(r1, s1, intfName1='router-eth0', params={'ip': '10.0.1.1/24'})
+    net.addLink(rc, r1, intfName1='router-eth0', intfName2='router-eth1')
 
-        info('*** Add links switch\n')
-        self.addLink(h1, s1)
-        self.addLink(h2, s1)
-        self.addLink(r1, s1, intfName1='r1-eth0', params1={'ip': '10.0.1.1/24'})
-        r1.cmd('sysctl -w net.ipv4.ip_forward=1')
+    # Configurar las interfaces del router
+    rc.cmd('ifconfig router-eth0 192.168.100.6 netmask 255.255.255.248')
+    rc.cmd('ifconfig router-eth1 192.168.100.14 netmask 255.255.255.248')
 
+    r1.cmd('ifconfig router-eth0 10.0.1.1 netmask 255.255.255.0')
+    r1.cmd('ifconfig router-eth1 192.168.100.1 netmask 255.255.255.248')
 
-def myNetwork():
-    topo = CustomTopology()
-    net = Mininet(topo=topo, controller=None)
+    net.build()
     net.start()
 
-    
-    
-    r1 = net.get('r1')
-    r1.cmd('ip route add 10.0.1.0/24 via 192.168.100.1 dev r1-eth0')
+    # Configurar rutas estáticas en el router si es necesario
+    rc.cmd('route add default gw 192.168.100.6 router-eth0')
+    rc.cmd('route add default gw 192.168.100.14 router-eth1')
 
-    info( '*** Starting controllers\n')
-    for controller in net.controllers:
-        controller.start()
+    r1.cmd('route add default gw 10.0.1.1 router-eth0')
+    r1.cmd('route add default gw 192.168.100.1 router-eth1')
 
-    info( '*** Starting switches\n')
-    net.get('s1').start([])
+    h1.cmd('route add default gw 10.0.1.1')
+    h2.cmd('route add default gw 10.0.1.1')
+
+    # Enrutamiento entre las redes
+    rc.cmd('ip route add 10.0.1.0/24 via 192.168.100.1 dev router-eth0')
 
     CLI(net)
-
     net.stop()
 
 if __name__ == '__main__':
-    setLogLevel('info')
-    myNetwork()
+    create_topology()
